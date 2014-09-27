372f866663893ef254e35349863109e7358c55c0
Joel Sommers <jsommers@acm.org>
2014-09-27 09:57:59 -0400

    Blank slate for main.c

--- main.c
+++ main.c
@@ -12,29 +12,10 @@
 #include <signal.h>
 
 
-void process_line(char *input_line) {
-    // your code should start here;  the line below
-    // is just to help you see the exact text that gets typed in from the
-    // loop in main().  
-    printf("You typed <%s>\n", input_line);
-}
+int main(int argc, char **argv) {
 
-
-#ifndef AUTOTEST
-int main(int argc, char **argv)
-{
-    char buffer[1024];
-    for (;;) { // infinite loop
-        memset(buffer, 0, 1024);
-        printf(">> "); // the shell prompt
-        fflush(stdout);
-        if (fgets(buffer, 1024, stdin) == NULL){
-            break;
-        }
-        process_line(buffer);
-    } 
+    // a blank slate :-)
 
     return 0;
 }
-#endif // AUTOTEST
 
